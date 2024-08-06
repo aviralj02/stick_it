1e465f3fa6ae99e482a0cf5a5087965ba4d568de
SanyamPunia <lewarends@gmail.com>
2024-08-07 03:23:07 +0530

    feat: :lipstick: init hero + load framer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="overflow-scroll">
-      <code>init nav</code>
+    <MaxWidthWrapper>
+      <Hero />
     </MaxWidthWrapper>
   );
 }
