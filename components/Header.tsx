360ad7567ee44004041b282417c9a8b74cb3bdd3
SanyamPunia <lewarends@gmail.com>
2024-08-06 15:38:02 +0530

    feat: :sparkles: init nav with framer-motion

---
+++ components/Header.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { Sparkles } from "lucide-react";
+import Image from "next/image";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  const [navStyle, setNavStyle] = useState("");
+  const [logoStyle, setLogoStyle] = useState("");
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    const previous = scrollY.getPrevious()!;
+
+    console.log(latest);
+
+    if (latest > previous && latest > 150) {
+      setNavStyle("border border-border bg-secondary rounded-full px-3");
+      setLogoStyle("hidden");
+    } else {
+      setNavStyle("");
+      setLogoStyle("")
+    }
+  });
+
+  return (
+    <motion.div
+      className={cn(
+        `flex max-w-screen-xl mx-auto sticky top-12 justify-between items-center my-12 py-2 transition-all`,
+        navStyle
+      )}
+      transition={{
+        duration: 0.1,
+        ease: "easeInOut",
+      }}
+    >
+      <div>
+        <Link href="/" className="flex gap-3 items-center">
+          <Image height={30} width={30} src="/logo.png" alt="logo" />
+          <h1
+            className={cn(
+              logoStyle,
+              `font-sans tracking-tight cursor-pointer font-bold text-2xl`
+            )}
+          >
+            stick it.
+          </h1>
+        </Link>
+      </div>
+      <div>
+        <Button className="rounded-full items-center gap-2" size="sm">
+          <Sparkles height={15} width={15} fill="#fff" />
+          Start a project
+        </Button>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Header;
