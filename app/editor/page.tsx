815f407acd0b940a7f5f0b641b24a107b4f8b10e
Aviral Jain <aviraljain1311@gmail.com>
2024-11-02 00:15:06 +0530

    fix: :bug: disable ssr for FullScreenEditor which was causing hydration issues

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,4 +1,11 @@
-import FullScreenEditor from "@/components/editor/FullScreenEditor";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const FullScreenEditor = dynamic(
+  () => import("@/components/editor/FullScreenEditor"),
+  { ssr: false }
+);
 
 export default function Editor() {
   return <FullScreenEditor />;
