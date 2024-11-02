34c53b0a5d5b59efbb050420993a6090ed3471c7
Aviral Jain <aviraljain1311@gmail.com>
2024-11-02 15:02:51 +0530

    feat: :chart_with_upwards_trend: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -27,6 +28,8 @@ export default function RootLayout({
         )}
       >
         {children}
+
+        <Analytics />
       </body>
     </html>
   );
